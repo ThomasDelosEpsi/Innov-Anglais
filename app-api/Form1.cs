2f93569dcdebea5b5fa137c75b16b26e4466c581
Luciasss <94835756+Luciass@users.noreply.github.com>
2023-05-02 18:01:55 +0200

    test connexion pour token

--- app-api/Form1.cs
+++ app-api/Form1.cs
@@ -11,13 +11,18 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Collections.ObjectModel;
 using System.Net.Http;
-using System.Net.Http.Headers;
+using System.Net.Http.Json;
 using System.Runtime;
+using System.Text.Json.Nodes;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+//using System.Text.Json;
 
 namespace app_api
 {
     public partial class Form1 : Form
     {
+        string token = "0";
         public Form1()
         {
             InitializeComponent();
@@ -27,29 +32,59 @@ namespace app_api
         {
             using (HttpClient client = new HttpClient())
             {
-                try
-                {
                     string uri = $"https://s4-8001.nuage-peda.fr/Innov-Anglais/public/api/users";
-                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2ODI2NzczMjksImV4cCI6MTY4MjY4MDkyOSwicm9sZXMiOlsiUk9MRV9BRE1JTiIsIlJPTEVfVVNFUiJdLCJ1c2VybmFtZSI6ImNoYW50YWwuY2hhdXZldEBub29zLmZyIn0.7Ckbqwhn67rtTi9eBtUiT2ootlk1jTX_hEc6qLWPuKZrbGK2QauSqEitPyVpQxofyy-CZHJDW84K0wStLe4CkrVEHZvVgh7fsy-MANnivTGDYu1DYKdrZt80Li-tof2nOFdE0qr4Hx540fy2dRwnTU4yyDZ5-wA2T_sOCBAqVBdgFa_fPjY5amSJaagQJRjZvfmQP2xAmciI3b7ob5sQa09z9ln_TxLX5cjWPqto9_jp2CCFzt2SPyI184khgiqGgfUWGM5RGzOry7buFlhc3P9MOTyXk9g0pfdA1v8Ufc_7cmlqNfVj8J9hIS6W7hi7oQ21KH0Ebks82m1Dsr6d-g");
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
                     HttpResponseMessage response = await client.GetAsync(uri);
                     if (response.IsSuccessStatusCode)
                     {
                         string json = await response.Content.ReadAsStringAsync();
-                        ListUser user = JsonConvert.DeserializeObject<ListUser>(json);
-                        foreach (Users utilisateur in user.utilisateur)
+                        JObject user = JsonConvert.DeserializeObject<JObject>(json);
+                        JArray listUsers = (JArray)user["hydra:member"];
+                    foreach (JObject utilisateur in listUsers)
+                    {
+                        label1.Text += "\n" + utilisateur["firstname"];
+                        label2.Text += "\n" + utilisateur["lastname"];
+                        if ((string)utilisateur["sex"] == "false")
                         {
-                            MessageBox.Show(utilisateur.firstname);
+                            label3.Text += "\nHomme";
+                        } else
+                        {
+                            label3.Text += "\nFemme";
+                        }
+                        label4.Text += "\n" + utilisateur["phone"];
+                        label5.Text += "\n" + (string)utilisateur["subscribe"]["type_subscribe"];
+                        label6.Text += "\n" + (string)utilisateur["subscribe"]["price"];
                         }
                     } else
                     {
                         MessageBox.Show($"Error: {response.StatusCode}");
                     }
-                }
-                catch(Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
             }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            /*using (HttpClient client = new HttpClient())
+            {
+                Token userToken = new Token
+                {
+                    email = textBox1.Text,
+                    password = textBox2.Text
+                };
+
+                var postContent = new StringContent(
+                JsonSerializer.Serialize(Stream, userToken),
+                Encoding.UTF8,
+                "application/json"
+                );
+
+                var response = await client.PostAsync("oauth/login", postContent);
+                if (!response.IsSuccessStatusCode)
+                    throw new Exception("Third party API request failed");
+
+                await using var stream = await response.Content.ReadAsStreamAsync();
+                return JsonSerializer.DeserializeAsync<Token>(stream);
+            }*/
         }
     }
 }
